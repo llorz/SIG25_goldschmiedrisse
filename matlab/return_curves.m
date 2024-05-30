6fd22c4030c1889876cc34b935aa99776dc58265
Jing Ren <jing.ren@kaust.edu.sa>
2024-05-30 14:42:07 +0200

    group 1 with 4-sym

---
+++ matlab/return_curves.m
@@ -0,0 +1,150 @@
+function cs = return_curves(name)
+if strcmpi(name, 'tn-1')
+    anchor = [-1, 0; ...
+        0.7, -0.7];
+    anchor_label = [0, 1];
+    uc1 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        4, true);
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc1);
+    return;
+end
+
+if strcmpi(name, 'tn-2')
+    anchor = [-1, 0; ...
+        0.7, -0.7];
+    anchor_label = [1,0];
+    uc2 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        4, true);
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc2);
+    return;
+end
+
+if strcmpi(name, 'tn-3')
+    anchor = [-0.8, 0;
+        0, -0.5];
+    anchor_label = [0,1];
+    uc3 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        4, true);
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc3);
+    return;
+end
+
+if strcmpi(name, 'tn-3.v1')
+    anchor = [-0.8, 0;
+        -0.1, -0.5;
+        0.1, -0.4];
+    anchor_label = [0,1,1];
+    uc5 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        4, true);
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc5);
+    return;
+end
+
+if strcmpi(name, 'tn-4.v1')
+    anchor = [-0.5, 0;
+        -0.1, -0.8;
+        0.1, -0.6];
+    anchor_label = [0,1,1];
+    uc6 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        4, true);
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc6);
+    return;
+end
+
+
+if strcmpi(name, 'tn-4')
+    anchor = [-0.8, 0;
+        0, -0.5];
+    anchor_label = [1,0];
+    uc4 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        4, true);
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc4);
+    return;
+end
+
+
+if strcmpi(name, 'tn-7')
+    anchor = [0, -0.4; ...
+        0.8, 0];
+    anchor_label = [0,1];
+    uc1 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        2, true);
+
+
+    anchor = [-0.5, 0;
+        -0.1, -0.8;
+        0.1, -0.6];
+    anchor_label = [0,1,1];
+    uc2 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        2, true);
+
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc1);
+    cs.add_unit_curve(uc2);
+    return
+end
+
+if strcmpi(name, 'tn-8')
+    anchor = [-0.3, -0.2;
+        0.45, -0.5;
+        0.5, -0.1];
+    anchor_label = [0,1,1];
+    uc8 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        3, true);
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc8);
+    return
+end
+
+if strcmpi(name, 'tn-9')
+    anchor = [-0.6, 0;
+        -0.3, -0.6;
+        0.1, -0.3];
+    anchor_label = [0,1,1];
+    uc9 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        4, true);
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc9);
+    return
+end
+
+
+if strcmpi(name, 'u.xi.21')
+    anchor = [-0.5, 0;
+        0.3, -0.45;
+        0.32, -0.2];
+    anchor_label = [0,1,1];
+    uc = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        4, true);
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc);
+    return
+end
+
+if strcmpi(name, 'u.xi.16')
+    anchor = [0, -0.4; ...
+        0.8, 0];
+    anchor_label = [0,1];
+    uc1 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        2, true);
+
+
+    anchor = [-0.5, 0;
+        -0.3, -0.5;
+        0.1, -0.2];
+    anchor_label = [0,1,1];
+    uc2 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
+        2, true);
+
+    cs = CurveStructure(name);
+    cs.add_unit_curve(uc1);
+    cs.add_unit_curve(uc2);
+    return
+end
+end
--- matlab/test_teaching_notes.m
+++ matlab/test_teaching_notes.m
@@ -1,96 +1,39 @@
 clc; clear; clf;
 addpath("utils/");
+%%
+group1 = {'tn-3', 'tn-4', 'tn-3.v1', 'tn-4.v1', 'tn-7', 'tn-9', 'U.XI.16', 'U.XI.21'};
 
-
-%%
-anchor = [-1, 0; ...
-   0.7, -0.7];
-anchor_label = [0, 1]; 
-uc1 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
-    4, true);
-
-%%
-anchor = [-1, 0; ...
-   0.7, -0.7];
-anchor_label = [1,0];
-uc2 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
-    4, true);
-
-%%
-anchor = [-0.8, 0;
-    0, -0.5];
-anchor_label = [0,1];
-uc3 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
-    4, true);
-%%
-anchor = [-0.8, 0;
-    0, -0.5];
-anchor_label = [1,0];
-uc4 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
-    4, true);
-
-%%
-anchor = [-0.8, 0;
-    -0.1, -0.5;
-    0.1, -0.4];
-anchor_label = [0,1,1];
-uc5 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
-    4, true);
-
-%%
-anchor = [-0.5, 0;
-    -0.1, -0.8;
-    0.1, -0.6];
-anchor_label = [0,1,1];
-uc6 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
-    4, true);
-%%
-anchor = [-0.6, 0;
-    -0.3, -0.6;
-    0.1, -0.3];
-anchor_label = [0,1,1];
-uc7 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
-    4, true);
-%%
-anchor = [-0.5, 0;
-    0.3, -0.45;
-    0.32, -0.2];
-anchor_label = [0,1,1];
-uc8 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
-    4, true);
-
-%%
-anchor = [-0.3, -0.2;
-    0.45, -0.5;
-    0.5, -0.1];
-anchor_label = [0,1,1];
-uc9 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
-    3, true);
-
-figure(2);
-uc = [uc1; uc2; uc3; uc4; uc5; uc6; uc7; uc8; uc9];
-figure(1); clf;
-for ii = 1:length(uc)
-    subplot(2,5,ii);  plot(uc(ii));
+num = ceil(length(group1)/2);
+figure(3); clf;
+for ii = 1:length(group1)
+    subplot(2,num,ii); 
+    cs = return_curves(group1{ii});
+    cs.plot_2D_projection();
 end
+
 %%
-anchor = [-1, 0; ...
-   0.7, -0.7];
-anchor_label = [1, 0]; 
+anchor = [0, -0.4; ...
+   0.8, 0];
+anchor_label = [0,1]; 
 uc1 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
     2, true);
 
 
-anchor = [-0.8, 0;
-    -0.1, -0.5;
-    0.1, -0.4];
+anchor = [-0.5, 0;
+    -0.3, -0.5;
+    0.1, -0.2];
 anchor_label = [0,1,1];
 uc2 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
     2, true);
 
 cs = CurveStructure();
-% cs.add_unit_curve(uc1);
+cs.add_unit_curve(uc1);
 cs.add_unit_curve(uc2);
-figure(1); clf;
+figure(2); clf;
 cs.plot_2D_projection();
 
+
+%%
+cs = return_curves('tn-9');
+figure(2); clf; 
+cs.plot_2D_projection();
