bb4d5bd66703ca84fb83b97693688c858ad7846d
Jing Ren <jing.ren@kaust.edu.sa>
2024-05-30 09:57:36 +0200

    3-symm

--- matlab/test_teaching_notes.m
+++ matlab/test_teaching_notes.m
@@ -60,27 +60,13 @@ uc8 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
     4, true);
 
 %%
-anchor = [-0.3, -0.1;
-    0.5, -0.45;
-    0.6, -0.2];
+anchor = [-0.3, -0.2;
+    0.45, -0.5;
+    0.5, -0.1];
 anchor_label = [0,1,1];
 uc9 = UnitCurve(ControlledCurve(anchor, [], anchor_label), ...
-    1, false);
-%%
-p0 = [-0.3, -0.1];
-p = p0'/norm(p0);
-ref_mat = 2 * (p * p') - eye(2);
+    3, true);
 
-p1 = anchor;
-p2 = (p1-p0)*ref_mat'+p0;
-p2 = p1*ref_mat';
-
-figure(2); clf
-plot(p1(:,1), p1(:,2)); hold on;
-plot(p2(:,1), p2(:,2));
-axis equal;
-plot([-0.3;0],[-0.1;0])
-%%
 uc = [uc1; uc2; uc3; uc4; uc5; uc6; uc7; uc8; uc9];
 figure(1); clf;
 for ii = 1:length(uc)
